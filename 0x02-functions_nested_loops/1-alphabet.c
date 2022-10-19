b577b7468264ad8848c714e9771c741e45e23988
Sherifat Idris <idrissherifat06@gmail.com>
2022-10-18 17:20:41 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+
+/**
+ *  * print_alphabet 
+ *
+ *   * Description: It prints the alphabet in lowercase followed by a new line
+ *
+ *    * No Return
+ */
+
+void print_alphabet(void)
+
+{
+
+		char i;
+
+
+
+			for (i = 'a'; i <= 'z'; i++)
+
+						_putchar(i);
+
+				_putchar('\n');
+
+}
