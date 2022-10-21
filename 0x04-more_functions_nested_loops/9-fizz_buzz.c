23135733ff35718f2564c4fd887734bd110c28e2
Sherifat Idris <idrissherifat06@gmail.com>
2022-10-21 02:32:42 -0700

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,14 +18,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < = 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			print("Fizz");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			print("Buzz");
+			printf("Buzz");
 		else if (i % 15 == 0)
-			print("FizzBuzz");
+			printf("FizzBuzz");
 		else
 			printf("%i", i);
 		if (i < 100)
