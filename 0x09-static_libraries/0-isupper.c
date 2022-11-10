63fdd7f2a3f18bf6c304572fbe1bbfd7bf680164
Sherifat Idris <idrissherifat06@gmail.com>
2022-11-09 17:12:27 -0800

    static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+
+
+int main(void)
+{
+	int a = 3;
+
+	_isupper(a);
+	return (0);
+}
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * @c: an integer
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	int c = 5;
+
+	_isdigit(c);
+	return (0);
+}
